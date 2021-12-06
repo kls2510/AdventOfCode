64d43d8c727beba53576699ef5cdd5c9b3fd9c1e
kls2510 <scottk008L@hotmail.co.uk>
2021-12-06 00:37:49 +0000

    optimise day 5

--- src/main/scala/advent_of_code2021/exercises/Day5.scala
+++ src/main/scala/advent_of_code2021/exercises/Day5.scala
@@ -21,40 +21,40 @@ case class Line(start: Point, end: Point) extends WithLogger{
     val maxY: Int = List(start.y, end.y).max
     val minX: Int = List(start.x, end.x).min
     val minY: Int = List(start.y, end.y).min
+    val xList = List.range(minX, maxX + 1)
+    val yList = List.range(minY, maxY + 1)
+    val hor = end.x > start.x
+    val ver = end.y > start.y
+    val xyList = (hor, ver) match {
+        case (true, true) => xList zip yList
+        case (true, false) => xList zip yList.reverse
+        case (false, true) => xList.reverse zip yList
+        case (false, false) => xList.reverse zip yList.reverse
+    }
 
-    def markNonDiagonalsOnGrid(grid: List[List[Int]]): List[List[Int]] = {
+    def markNonDiagonalsOnGrid(grid: Map[(Int, Int), Int]): Map[(Int, Int), Int] = {
         (start, end) match {
             case (Point(x1, y1), Point(x2, y2)) if x1 == x2 => {
                 val colNo = x1
-                List.range(minY, maxY + 1).foldLeft(grid)(
-                    (newRows, y) => newRows.updated(y, newRows(y).updated(colNo, newRows(y)(colNo) + 1))
+                yList.foldLeft(grid)(
+                    (newGrid, y) => newGrid + ((colNo, y) -> (newGrid.getOrElse((colNo, y), 0) + 1))
                 )
             }
             case (Point(x1, y1), Point(x2, y2)) if y1 == y2 => {
                 val rowNo = y1
-                List.range(minX, maxX + 1).foldLeft(grid)(
-                    (newRows, x) => newRows.updated(rowNo, newRows(rowNo).updated(x, newRows(rowNo)(x) + 1))
+                xList.foldLeft(grid)(
+                    (newGrid, x) => newGrid + ((x, rowNo) -> (newGrid.getOrElse((x, rowNo), 0) + 1))
                 )
             }
             case _ => grid
         }
     }
 
-    def markAllOnGrid(grid: List[List[Int]]): List[List[Int]] = {
+    def markAllOnGrid(grid: Map[(Int, Int), Int]): Map[(Int, Int), Int] = {
         (start, end) match {
             case (Point(x1, y1), Point(x2, y2)) if x1 != x2 && y1 != y2 => {
-                val hor = x2 > x1
-                val ver = y2 > y1
-                val xList = List.range(minX, maxX + 1)
-                val yList = List.range(minY, maxY + 1)
-                val coords = (hor, ver) match {
-                    case (true, true) => xList zip yList
-                    case (true, false) => xList zip yList.reverse
-                    case (false, true) => xList.reverse zip yList
-                    case (false, false) => xList.reverse zip yList.reverse
-                }
-                coords.foldLeft(grid)(
-                    (newRows, coord) => newRows.updated(coord._2, newRows(coord._2).updated(coord._1, newRows(coord._2)(coord._1) + 1))
+                xyList.foldLeft(grid)(
+                    (newGrid, coord) => newGrid + (coord -> (newGrid.getOrElse(coord, 0) + 1)) 
                 )
             }
             case _ => markNonDiagonalsOnGrid(grid)
@@ -76,35 +76,27 @@ object Day5 extends WithLogger with Exercise[(List[Line], Int, Int)] {
         (lines, lines.map(_.maxX).max, lines.map(_.maxY).max)
     }
 
-    def genGrid(width: Int, height: Int): List[List[Int]] = List.range(0, height + 1).map(
-        _ => {
-            List.range(0, width + 1).map(
-                _ => 0
-            )
-        }
-    )
-
     override def part1(input: (List[Line], Int, Int)): Unit = {
         logInfo("Starting part 1")
         
-        val grid = genGrid(input._2, input._3)
+        val grid: Map[(Int, Int), Int] = Map()
         val finalGrid = input._1.foldLeft(grid)(
             (grid, line) => {
                 line.markNonDiagonalsOnGrid(grid)
             }
         )
-        logInfo("Score = " + finalGrid.flatten.filter(_ > 1).map(_ => 1).reduce(_ + _).toString)
+        logInfo("Score = " + finalGrid.values.filter(_ > 1).map(_ => 1).reduce(_ + _).toString)
     }
 
     override def part2(input: (List[Line], Int, Int)): Unit = {
         logInfo("Starting part 2")
 
-        val grid = genGrid(input._2, input._3)
+        val grid: Map[(Int, Int), Int] = Map()
         val finalGrid = input._1.foldLeft(grid)(
             (grid, line) => {
                 line.markAllOnGrid(grid)
             }
         )
-        logInfo("Score = " + finalGrid.flatten.filter(_ > 1).map(_ => 1).reduce(_ + _).toString)
+        logInfo("Score = " + finalGrid.values.filter(_ > 1).map(_ => 1).reduce(_ + _).toString)
     }
 }
